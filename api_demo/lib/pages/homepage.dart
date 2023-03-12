f1df6d9b4063fd2837fd176877ffa0870cc156d7
Yashkant103 <100015305+Yashkant103@users.noreply.github.com>
2023-03-12 17:19:34 +0530

    Simple_Api_Project

---
+++ api_demo/lib/pages/homepage.dart
@@ -0,0 +1,102 @@
+import 'package:api_demo/model/student.dart';
+import 'package:api_demo/pages/studentpage.dart';
+import 'package:api_demo/services/remoteservices.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Api Home Page"),
+      ),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(
+                semanticsLabel: "Loading Data",
+                backgroundColor: Colors.grey,
+              ),
+            );
+          } else {
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(snapshot.data![index].avatar),
+                  ),
+                  title: Text(snapshot.data![index].name),
+                  trailing: IconButton(
+                    onPressed: () {
+                      RemoteServices.deleteStudent(
+                          id: snapshot.data![index].id);
+                      setState(() {});
+                    },
+                    icon: const Icon(Icons.delete),
+                    color: Colors.red,
+                  ),
+                  subtitle: Text("Email : ${snapshot.data![index].email}"),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StudentPage(
+                        isedit: true,
+                        obj: snapshot.data![index],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+        future: RemoteServices.getAllStudent(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const StudentPage(isedit: false),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+// InkWell(
+//  child: Icon(
+//    (!getBoolFromDynamic(snapshot.data!, index))
+//        ? Icons.favorite_border
+//        : Icons.favorite,
+//    color: Colors.amber),
+//  onTap: () async {
+//    http.Response res = await updateFavorite(
+//        (jsonDecode(snapshot.data!.body.toString())[
+//         index]['id']),
+//         !getBoolFromDynamic(snapshot.data!, index));
+//     if (res.statusCode == 200) {
+//        setState(() {});
+//      }
+//    },
+// ),
---
+++ api_demo/lib/utils.dart
@@ -0,0 +1,24 @@
+// Replace Your Url
+var url = "https://630c473353a833c53426bfc3.mockapi.io/demo";
+Map<String, String> Headers = {"Content-Type": "application/json"};
+Uri APIURL = Uri.parse(url);
+
+
+
+// sample Data of one of My Json Data
+
+// Replace Your Json Schema according to Model class And Other Filed
+/* 
+
+[
+    {
+        "name": "Holly Brekke",
+        "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/849.jpg",
+        "rollnumber": 40345,
+        "email": "Deja9@hotmail.com",
+        "address": "482 Lockman Shoals",
+        "id": "1"
+    },
+]
+
+ */
